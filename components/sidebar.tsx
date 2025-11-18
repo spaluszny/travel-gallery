55fbc77978fa828542b74775e1cab67757c3f1ad
Sarah Paluszny <sarah.paluszny@gmail.com>
2025-11-18 16:54:21 -0500

    build: icons

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,25 +1,48 @@
+import { siteConfig } from "@/config/site";
 import Link from "next/link";
+import { Icons } from "./icons"
 
 
 export default function SideBar() {
     return (
 
         <div className="h-screen w-100 bg-[#F5F5F5] p-10 fixed">
-            <div>
-                <h2>Sarah Paluszny</h2>
-                <h3 className="-mt-2">Travel Photography</h3>
-                <p className="pt-5">This is a collection of photographs, I’ve attained while traveling the world. Some are taken by me. Some are taken by friends I’ve met along the way. Hope you enjoy :)</p>
-            </div>
-            <div className="pt-20">
-                <h4 className="font-bold">The Gallery</h4>
-                <ul className="flex flex-col gap-1 text-md">
-                    <li className="underline hover:text-gray-600"><Link href="/north-america">North America</Link></li>
-                    <li className="underline hover:text-gray-600"><Link href="/asia">Asia</Link></li>
-                    <li className="underline hover:text-gray-600"><Link href="/europe">Europe</Link></li>
-                    <li className="underline hover:text-gray-600"><Link href="/africa">Africa</Link></li>
-                </ul>
-            </div>
+            <div className="">
 
+                <div>
+                    <h2>Sarah Paluszny</h2>
+                    <h3 className="-mt-2">Travel Photography</h3>
+                    <p className="pt-5">This is a collection of photographs, I’ve attained while traveling the world. Some are taken by me. Some are taken by friends I’ve met along the way. Hope you enjoy :)</p>
+                </div>
+                <div className="pt-10 text-lg"><Link href="/">Recent</Link></div>
+                <div className="pt-10">
+                    <h4 className="font-bold">The Gallery</h4>
+                    <ul className="flex flex-col gap-1 text-md">
+                        {/* <li className="underline hover:text-gray-600"><Link href="/">Recent</Link></li> */}
+                        <li className="underline hover:text-gray-600"><Link href="/north-america">North America</Link></li>
+                        <li className="underline hover:text-gray-600"><Link href="/asia">Asia</Link></li>
+                        <li className="underline hover:text-gray-600"><Link href="/europe">Europe</Link></li>
+                        <li className="underline hover:text-gray-600"><Link href="/africa">Africa</Link></li>
+                    </ul>
+                </div>
+                <div className="absolute bottom-10">
+                    <div className="flex gap-5">
+                        <Link href={`mailto:${siteConfig.links.email}`} target="blank" rel="noreferrer">
+                            <Icons.email className="h-8 w-8" />
+                            <span className="sr-only">Email</span>
+                        </Link>
+                        <Link href={siteConfig.links.github} target="blank" rel="noreferrer">
+                            <Icons.gitHub className="h-8 w-8" />
+                            <span className="sr-only">Github</span>
+                        </Link>
+                        <Link href={siteConfig.links.website} target="blank" rel="noreferrer">
+                            <Icons.website className="h-8 w-8" />
+                            <span className="sr-only">Personal Website</span>
+                        </Link>
+                    
+                    </div>
+                </div>
+            </div>
         </div>
 
 
